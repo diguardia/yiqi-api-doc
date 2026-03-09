0b247ec7288de4a6f1fd0f7e743a4d792a225df9
cdiguardia <cguardia@gmail.com>
2026-03-08 23:18:21 -0300

    primer commit

---
+++ swagger-initializer.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+      var url = window.location.search.match(/url=([^&]+)/);
+      if (url && url.length > 1) {
+        url = decodeURIComponent(url[1]);
+      } else {
+        url = "http://petstore.swagger.io/v2/swagger.json";
+      }
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: url,
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
